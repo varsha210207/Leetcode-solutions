58d4006b05ddb38fa22007ad3bc45648f146cc70
varsha210207 <vl668542@gmail.com>
2026-06-09 19:19:59 +0530

    Implement moveZeroes method to shift zeros

---
+++ Arrays/movezeros.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int k=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[k]=nums[i];
+                k++;
+            }    
+        }
+            while(k<nums.length)
+            {
+                nums[k]=0;
+                k++;
+            }
+        
+        }
+        
+    }
